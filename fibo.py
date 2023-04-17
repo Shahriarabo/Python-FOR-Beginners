21885491049b487581b3b8206a67f9874855aeeb
Abdullah al shahriar <103172072+Shahriarabo@users.noreply.github.com>
2023-04-17 11:16:00 +0600

    Add files via upload

--- fibo.py
+++ fibo.py
@@ -1,16 +1,28 @@
-def fibo(n):
-    if n== 0:
-        return 0
+
+#def fibo(n):
+ #   if n == 0:
+  #      return 0
+   # return 1 + fibo (n//10)
+
+#print(fibo(1234567891011121314151617181920))
+
+
+#def expolad(x ,y):
+#   if y == 0:
+ #       return 1
+  #  return x * expolad(x ,y-1)
+
+#print(expolad(2,3))
+
+
+
+
+def expolad(x ,y):
+    if y == 0:
+        return 1
+    elif y % 2 == 0:
+        return expolad(x,y//2)*expolad(x,y//2)
     else:
-        x=0
-        y=1
-        print(x)
-        print(y)
+        return x*expolad(x,y//2)*expolad(x,y//2)
 
-        for i in range(1,n):
-            w=x+y
-            x=y
-            y=w
-            print(w)
-
-fibo(6)
+print(expolad(9,4))
